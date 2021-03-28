fb06436327aedbe5f108e7aec94dc3e8c020162d
Joachim Laplanche <joachim.laplanche@gmail.com>
2021-03-28 20:57:22 +0200

    first instances of game

--- src/classes.js
+++ src/classes.js
@@ -23,16 +23,20 @@ class Hero {
     get collides() {
         var col = { up: false, down: false, left: false, right: false }
         var hL = this.x, hR = hL + this.width, hT = this.y, hB = hT + this.height;
-        var affX = x => (x - this.x) * this.speed.y / this.speed.x + this.y, affY = y => (y - this.y) * this.speed.x / this.speed.y + this.x
+        // var affX = x => (x - this.x) * this.speed.y / this.speed.x + this.y, affY = y => (y - this.y) * this.speed.x / this.speed.y + this.x
         Block.All.forEach(block => {
             var bL = block.x, bR = bL + block.width, bT = block.y, bB = bT + block.height;
             if (bT < hB && bB > hT) {
-                if (bL < hL && bR > hL) col.left = block;
-                if (bL < hR && bR > hR) col.right = block;
+                if (bR < hL && bR > hL + this.speed.x) col.left = block;
+                if (bL > hR && bL < hR + this.speed.x) col.right = block;
+                // if (bL < hL && bR > hL) col.left = block;
+                // if (bL < hR && bR > hR) col.right = block;
             }
             if (bL < hR && bR > hL) {
-                if (bT < hT && bB > hT) col.up = block;
-                if (bT < hB && bB > hB) col.down = block;
+                if (bB < hT && bB > hT + this.speed.y) col.up = block;
+                if (bT > hB && bT < hB + this.speed.y) col.down = block;
+                // if (bT < hT && bB > hT) col.up = block;
+                // if (bT < hB && bB > hB) col.down = block;
             }
         })
         return col
@@ -42,7 +46,25 @@ class Hero {
         var hL = this.x, hR = hL + this.width, hB = this.y + this.height;
         for (let b of Block.All) {
             var bL = b.x, bR = bL + b.width, bT = b.y;
-            if (bL < hR && bR > hL && Math.abs(bT - hB) < 3) return true
+            if (bL < hR && bR > hL && Math.abs(bT - hB) < Math.abs(this.speed.y + .1)) return true
+
+        }
+        return false
+    }
+    get goingIntoBlockLeft() {
+        var hL = this.x, hT = this.y, hB = hT + this.height;
+        for (let b of Block.All) {
+            var bR = b.x + b.width, bT = b.y, bB = bT + b.height;
+            if (bT < hB && bB > hT && Math.abs(bR - hL) < Math.abs(this.speed.x - 1)) return true
+
+        }
+        return false
+    }
+    get goingIntoBlockRight() {
+        var hR = this.x + this.width, hT = this.y, hB = hT + this.height;
+        for (let b of Block.All) {
+            var bL = b.x, bT = b.y, bB = bT + b.height;
+            if (bT < hB && bB > hT && Math.abs(bL - hR) < Math.abs(this.speed.x + 1)) return true
 
         }
         return false
